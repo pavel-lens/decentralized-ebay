d0724a5e377895507564e2142386ae36b21ddd0e
Pavel Svitek <svitek.pavel@gmail.com>
2018-09-03 23:07:23 +0200

    adjust deploy_migrations.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
 var EcommerceStore = artifacts.require("./EcommerceStore.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(EcommerceStore, web3.eth.accounts[9]);
+  deployer.deploy(EcommerceStore);
+  // deployer.deploy(EcommerceStore, web3.eth.accounts[9]);
 };
