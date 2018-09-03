e0f24ae235965cec4d4592ad0e01d4b30f595b7c
Pavel Svitek <svitek.pavel@gmail.com>
2018-09-03 23:08:20 +0200

    add truffle-console-commands.js

---
+++ truffle-console-commands.js
@@ -0,0 +1,1 @@
+EcommerceStore.deployed().then(function(f) { f.addProductToStore('iphone X', 'phones', 'imageLink', 'descLink', 1536007648, web3.toWei(1, 'ether'), 0) }.then(function(f) { console.log(f) } })
