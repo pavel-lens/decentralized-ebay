a8c58a4c160850cbf3b1b4b981896f9c73fec03f
Pavel Svitek <svitek.pavel@gmail.com>
2018-09-09 13:32:32 +0200

    implement displaying products

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,73 +1,74 @@
+/* eslint-disable max-len */
 // Import the page's CSS. Webpack will know what to do with it.
-import "../stylesheets/app.css";
+import '../stylesheets/app.css'
 
 // Import libraries we need.
-import { default as Web3} from 'web3';
+import { default as Web3 } from 'web3'
 import { default as contract } from 'truffle-contract'
 
-const ipfsAPI = require('ipfs-api');
-const ipfs = ipfsAPI({host: '127.0.0.1', port: '5001', protocol: 'http'});
-
 // Import our contract artifacts and turn them into usable abstractions.
-import ecommerce_store_artifacts from '../../build/contracts/EcommerceStore.json'
+import ecommerceStoreArtifacts from '../../build/contracts/EcommerceStore.json'
 
 // MetaCoin is our usable abstraction, which we'll use through the code below.
-var EcommerceStore = contract(ecommerce_store_artifacts);
-
-var reader;
-
-const categories = ["Art","Books","Cameras","Cell Phones & Accessories","Clothing","Computers & Tablets","Gift Cards & Coupons","Musical Instruments & Gear","Pet Supplies","Pottery & Glass","Sporting Goods","Tickets","Toys & Hobbies","Video Games"];
+var EcommerceStore = contract(ecommerceStoreArtifacts)
 
 window.App = {
-  start: function() {
-    var self = this;
+  start: function () {
+    var self = this
 
     // Bootstrap the MetaCoin abstraction for Use.
-    EcommerceStore.setProvider(web3.currentProvider);
-
+    EcommerceStore.setProvider(web3.currentProvider)
+    renderStore()
   }
-};
-
-function renderProductDetails(productId) {
 }
 
-function saveProduct(product) {
-  // 1. Upload image to IPFS and get the hash
-  // 2. Add description to IPFS and get the hash
-  // 3. Pass the 2 hashes to addProductToStore
-  
+function renderStore () {
+  var instance
+  EcommerceStore.deployed()
+    .then((f) => {
+      instance = f
+      return instance.productIndex.call()
+    })
+    .then((count) => {
+      for (var i = 1; i <= count; i++) {
+        renderProduct(instance, i)
+      }
+    })
 }
 
-function saveImageOnIpfs(reader) {
+function renderProduct (instance, proudctId) {
+  instance.getProduct.call(proudctId).then((product) => {
+    const node = $('<div />')
+    console.log(product);
+    node.addClass('col-sm-3 text-center col-margin-bottom-1 product')
+    node.append(`<div class="title">${product[1]}</div>`)
+    node.append(`<div class="">${displayPrice(product[6])}</div>`)
+
+    // Check if product was bought already
+    if (product[8] === '0x0000000000000000000000000000000000000000') {
+      $('#product-list').append(node)
+    } else {
+      $('#product-purchased').append(node)
+    }
+  })
 }
 
-function saveTextBlobOnIpfs(blob) {
+function displayPrice (amount) {
+  return `&Xi; ${web3.fromWei(amount, 'ether')}`
 }
 
-function renderStore() {
-}
-
-/*function renderProduct(instance, index) {
-}*/
-
-function renderProduct(product) {
-}
-
-function displayPrice(amt) {
-  return "&Xi;" + web3.fromWei(amt, 'ether');
-}
-
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof web3 !== 'undefined') {
     console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
     // Use Mist/MetaMask's provider
-    window.web3 = new Web3(web3.currentProvider);
+    // You have to select local server in Metamask!
+    window.web3 = new Web3(web3.currentProvider)
   } else {
-    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask")
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+    window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'))
   }
 
-  App.start();
-});
+  window.App.start()
+})
