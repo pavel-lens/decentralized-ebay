1e48712fe148b8a42e0f0a67c1d5aaea19b81b54
Pavel Svitek <svitek.pavel@gmail.com>
2018-09-09 14:20:09 +0200

    implement buy() function in smart contract

--- contracts/EcommerceStore.sol
+++ contracts/EcommerceStore.sol
@@ -47,5 +47,19 @@ contract EcommerceStore {
             return (product.id, product.name, product.category, product.imageLink,
                 product.descLink, product.startTime, product.price, product.condition,
                 product.buyer);
-        }
+    }
+
+    function buy(uint _productId) payable public {
+        Product memory product = stores[productIdInStore[_productId]][_productId];
+
+        // Check if product is not purchased yet
+        require(product.buyer == address(0));
+
+        // Check if enough Ether was sent to buy
+        require(msg.value >= product.price);
+
+        // Update owner and then save to store
+        product.buyer = msg.sender;
+        stores[productIdInStore[_productId]][_productId] = product;
+    }
 }
